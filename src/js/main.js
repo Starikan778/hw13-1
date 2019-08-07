c6b47b5768172c65a72b719860532d17fca68ace
Kostia <jorjeivan2@gmail.com>
2019-08-07 13:51:28 -0500

    переписали файлы js на jquery,сделали всплывающее окно на верхней кнопке

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
-var close = document.querySelector('#close')
+var close = document.querySelector('#close');
 
 button.addEventListener('click', function() {
   modal.classList.add('modal_active');
